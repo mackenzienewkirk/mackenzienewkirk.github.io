55be7886330246ca4f8c950ab9b9122b8133e24c
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-02-25 22:45:59 -0600

    adding app.js and styling

---
+++ app.js
@@ -0,0 +1,23 @@
+import { Card } from '@mui/material';
+function app() {
+  return(
+    <Card sx={{
+      textAlign: 'left',
+                fontSize: 25,
+                backgroundColor: '#AEC9FF',
+                color: '#410064',
+                lineHeight: 1,
+                fontFamily: 'Rugrats Sans',
+                padding: 2,
+                marginLeft: 6,
+                marginRight: 6,
+                lineHeight: 1,
+                textAlign: 'left',
+                borderRadius: 4,
+    }}>Mackenzie Newkirk
+      Software Engineer
+    </Card>
+  )
+}
+
+export default app;
