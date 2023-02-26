80d80eb6a078820f127b63e5a1e0b47da0e5698c
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-02-26 14:39:16 -0600

    changes to full site

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-import { Card } from '@mui/material';
-function index() {
-  return(
-    <Card sx={{
-      textAlign: 'left',
-                fontSize: 25,
-                backgroundColor: '#AEC9FF',
-                color: '#410064',
-                lineHeight: 1,
-                fontFamily: 'Rugrats Sans',
-                padding: 2,
-                marginLeft: 6,
-                marginRight: 6,
-                lineHeight: 1,
-                textAlign: 'left',
-                borderRadius: 4,
-    }}>Mackenzie Newkirk
-      Software Engineer
-    </Card>
-  )
-}
 
-export default index;
+// function index() {
+//   return()
+// }
+
+// export default index;
